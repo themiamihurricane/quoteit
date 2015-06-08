391fdb294f8bab384a8d1a9620d2ba84651166d4
Sherman Hewitt <s.hewitt7@umiami.edu>
2015-06-07 23:40:25 -0400

    Fixed spacing

--- app/js/app.js
+++ app/js/app.js
@@ -34,10 +34,10 @@ var savePoster = function(){
 	  	$poster.hide();
 	    $preview.append(canvas);
 	    var img = canvas.toDataURL();
-        var a = $("<a>").attr("href", img).attr("download", "quote-" + filename + ".png").appendTo("body");
+      var a = $("<a>").attr("href", img).attr("download", "quote-" + filename + ".png").appendTo("body");
 	  	a[0].click();
-        a.remove();
-        $preview.addClass('no-edit');
+      a.remove();
+      $preview.addClass('no-edit');
 	  }
 	});
 
